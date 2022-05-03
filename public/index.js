f2adaad39bcf639dcdc3e10783a0cb275e2bb625
Papon W <papon0102@gmail.com>
2022-05-03 15:05:29 +0700

    1st Draft

---
+++ public/index.js
@@ -0,0 +1,16 @@
+
+var today = (new Date ());
+document.getElementById("date").innerHTML= today.toLocaleDateString("en-US")
+
+
+
+
+console.log(today.toLocaleDateString("en-US")); // 9/17/2016
+
+
+
+
+
+
+
+
